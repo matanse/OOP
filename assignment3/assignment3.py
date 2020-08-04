8281fe28c5d139b30f95d92fa7f7450c0a7d9dc9
Matan Selig <matanselig@MacBook-Pro-6.local>
2020-08-04 11:51:18 +0200

    assignment3 done

--- assignment3/assignment3.py
+++ assignment3/assignment3.py
@@ -1,31 +1,20 @@
+import os
+
+
 class ConfigDict(dict):
     def __init__(self, file_name):
-        self.__file_name = file_name
-        try:
-            file = open(self.__file_name, 'r')
-        except IOError:
-            print("IOError: file doesn't exist")
-            quit()
-        print(file.readlines()[0].split(','))
+        self._file_name = file_name
+        if os.path.isfile(self._file_name):
+            with open(self._file_name) as file_handler:
+                for line in file_handler:
+                    line = line.rstrip()
+                    key, value = line.split('=', 1)
+                    dict.__setitem__(self, key, value)
+        else:
+            print('blaaaaaaaaaaaaa')
 
     def __setitem__(self, key, value):
         dict.__setitem__(self, key, value)
-        file = open(self.__file_name, 'a')
-        file.write('{0}:{1},'.format(key, value))
-        file.close()
-
-
-# dd = ConfigDict('dict.txt')
-# bb = ConfigDict('dict.txt')
-# dd['a'] = 1
-# dd['d'] = 1
-# dd['b'] = 1
-# print(dd)
-# bb['p'] = 2
-# bb['j'] = 2
-# bb['r'] = 2
-# print(dd)
-# print(bb)
-# pp = open('dict.txt', 'r')
-# for line in pp:
-#     print(line)
+        with open(self._file_name, 'w') as file_handler:
+            for key, value in self.items():
+                file_handler.write('{0} = {1}\n'.format(key, value))
--- assignment3/dictTest.py
+++ assignment3/dictTest.py
@@ -6,21 +6,28 @@
     ./dictTest.py wheel air  (sets wheel as key and air as value in the dict)
 """
 import sys
+import os
 from assignment3 import ConfigDict
 
-dict_file = ConfigDict('dict_1.txt')
+file_name = 'dict_1.txt'
+dict_file = ConfigDict(file_name)
 
 if len(sys.argv) == 3:
     key = sys.argv[1]
     value = sys.argv[2]
-    print('wrote "{0} = {1}" to a dict file.'.format(key, value))
+    print('writing "{0} = {1}" to a dict file.'.format(key, value))
     dict_file[key] = value
 else:
-    print('Reading dict file: ')
-    read_file = open('dict_1.txt', 'r')
-    for line in read_file.readlines()[0].split(','):
-        dict_line = line.split(':')
-        key = dict_line[0]
-        value = dict_line[1]
-        print(key + value)
-        # print('              {0} : {1}'.format(dict_line[0], dict_line[1]))
+    if os.path.isfile(file_name):
+        line_count = 0
+        print('Reading dict file: ')
+        with open(file_name) as df:
+            # print('        key   value')
+            for line in df:
+                line_count += 1
+                key, value = line.strip().split('=', 1)
+                print('        {0} : {1}'.format(key, value))
+            if line_count == 0:
+                print('       THE FILE IS EMPTY...')
+    else:
+        print('rrrrrrrrrr')
